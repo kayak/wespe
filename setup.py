a077a357f5543865ff7a281670fa3becc96fa897
Lucas Lira Gomes <llira@kayak.com>
2018-09-14 14:55:13 +0200

    Add main wespe package to find_package.

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     long_description=readme + '\n\n' + history,
     include_package_data=True,
     name='wespe',
-    packages=find_packages(include=['wespe.*']),
+    packages=find_packages(include=['wespe', 'wespe.*']),
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
