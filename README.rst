5474427559af4fff08e9ca71cfeabe96bd167c09
Lucas Lira Gomes <llira@kayak.com>
2020-01-30 18:43:30 +0100

    Bump version: 0.2.0 → 0.2.1

--- README.rst
+++ README.rst
@@ -36,7 +36,32 @@ To install |Brand| run the following command:
 .. _installation_end:
 
 
-.. include:: ./USAGE.rst
+Usage - Facebook Business
+-------------------------
+
+
+All steps from now on will assume you've already set the default api connection using facebook_busines. It's also
+possible to set one on the fly by providing the api kwarg in FacebookBatchUploader's constructor.
+
+
+.. code-block:: python
+
+    from wespe.batch_uploaders import FacebookBatchUploader
+
+    # There is no request limit. If necessary Wespe will coordinate the execution of multiple FacebookAdsApiBatch
+    # instances.
+    batch_uploader = FacebookBatchUploader(requests)
+
+    try:
+        batch_uploader.execute()
+    except BatchExecutionError:
+        for error in batch_uploader.errors:
+            # See FacebookBatchRequestError for more info on what you can do
+            pass
+
+    for response in batch_uploader.responses:
+        # See FacebookBatchResponse for more info on what you can do
+        pass
 
 
 License
--- setup.py
+++ setup.py
@@ -3,50 +3,46 @@
 
 """The setup script."""
 
-from setuptools import (
-    find_packages,
-    setup,
-)
+from setuptools import find_packages, setup
 
-from wespe import (
-    __author__,
-    __email__,
-    __version__,
-)
+from wespe import __author__, __email__, __version__
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('requirements.txt') as requirements_file:
+with open("requirements.txt") as requirements_file:
     requirements = requirements_file.read().splitlines()
 
-setup_requirements = ['pytest-runner', ]
+setup_requirements = ["pytest-runner"]
 
-test_requirements = ['pytest', ]
+test_requirements = ["pytest"]
 
 setup(
     author=__author__,
     author_email=__email__,
     classifiers=[
-        'License :: OSI Approved :: Apache Software License',
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        "License :: OSI Approved :: Apache Software License",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
-    keywords=('wespe adtech marketing facebook google bing yandex facebook'),
+    keywords=("wespe adtech marketing facebook google bing yandex facebook"),
     description="Batching ad tech providers’ operations for humans",
     install_requires=requirements,
     long_description=readme,
+    long_description_content_type="text/x-rst",
     include_package_data=True,
-    name='wespe',
-    packages=find_packages(include=['wespe', 'wespe.*'], exclude=['*.tests', '*.tests.*']),
+    name="wespe",
+    packages=find_packages(
+        include=["wespe", "wespe.*"], exclude=["*.tests", "*.tests.*"]
+    ),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/kayak/wespe',
+    url="https://github.com/kayak/wespe",
     version=__version__,
     zip_safe=False,
 )
